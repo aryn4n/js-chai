7294482158b70fa3c3fc7415a03abd9aa36ffcea
Aryn <aryan123sharma0@gmail.com>
2024-07-03 09:11:45 +0530

    change properties

---
+++ .vscode/CODES/JScode/03script.js
@@ -0,0 +1,8 @@
+console.log("Hello! I am Aryn and you are ?");
+
+let score = "45910nbc"
+const userId = 13579
+
+console.log(userId.toString());
+console.log(userId);
+console.log(score.indexOf());
